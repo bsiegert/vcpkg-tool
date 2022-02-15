cdf75aab991a03c74ffbff41b9a2966e29b495e7
nicole mazzuca <mazzucan@outlook.com>
2022-02-15 15:18:10 -0800

    one more try, then I ask for help

--- src/localization/Format-LocalizationFiles.ps1
+++ src/localization/Format-LocalizationFiles.ps1
@@ -1,15 +1,14 @@
-[CmdletBinding()]
-Param()
-
-$fileList = Get-Content -LiteralPath $Env:XLocFileList
+$fileList = Get-Content $Env:XLocFileList
 
 $fileList | % {
-    "" | Out-File -FilePath $fileName
-    #$fileName = $_
-    #$fileContents = Get-Content $fileName -Raw # removes the BOM
-    #$fileContents += "`n" # add a trailing newline
-    #$fileContents | Out-File `
-        #-FilePath $fileName `
-        #-Encoding UTF8NoBOM `
-        #-NoNewline
+    $fileName = $_
+    $fileContents = Get-Content $fileName -Raw # removes the BOM
+    if (-not $fileContents.EndsWith("`n"))
+    {
+        $fileContents += "`n" # add a trailing newline
+    }
+    $fileContents | Out-File `
+        -FilePath $fileName `
+        -Encoding UTF8NoBOM `
+        -NoNewline
 }
