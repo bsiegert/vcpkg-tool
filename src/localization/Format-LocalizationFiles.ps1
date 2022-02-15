62827278d98d1698e18e6beb957fb1c6dc54c22c
nicole mazzuca <mazzucan@outlook.com>
2022-02-15 15:08:29 -0800

    let's try DESTRUCTION

--- src/localization/Format-LocalizationFiles.ps1
+++ src/localization/Format-LocalizationFiles.ps1
@@ -1,20 +1,15 @@
 [CmdletBinding()]
 Param()
 
-Write-Warning "Using XLocFileList: $Env:XLocFileList"
 $fileList = Get-Content -LiteralPath $Env:XLocFileList
-Write-Warning "File list: ($($fileList -join ', '))"
 
 $fileList | % {
-    $fileName = $_
-    Write-Warning "Reading localization file: $fileName"
-    $fileContents = Get-Content $fileName -Raw # removes the BOM
-    $fileContents += "`n" # add a trailing newline
-    Write-Warning "Reading localization file: $fileName"
-    $fileContents | Out-File `
-        -FilePath $fileName `
-        -Encoding UTF8NoBOM `
-        -NoNewline
+    "" | Out-File -FilePath $fileName
+    #$fileName = $_
+    #$fileContents = Get-Content $fileName -Raw # removes the BOM
+    #$fileContents += "`n" # add a trailing newline
+    #$fileContents | Out-File `
+        #-FilePath $fileName `
+        #-Encoding UTF8NoBOM `
+        #-NoNewline
 }
-
-throw "hey does this even get run?"
