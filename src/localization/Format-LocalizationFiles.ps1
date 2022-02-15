badd5d748cf4560c8cb607d0d4ebc918afd1ad5f
nicole mazzuca <mazzucan@outlook.com>
2022-02-15 14:46:16 -0800

    does that work?

--- src/localization/Format-LocalizationFiles.ps1
+++ src/localization/Format-LocalizationFiles.ps1
@@ -1,13 +1,13 @@
 [CmdletBinding()]
 Param()
 
-$BasePath = Get-Item "$PSScriptRoot/../.."
-
 [String[]]$fileList = Get-Content -LiteralPath $Env:XLocFileList
 $fileList | % {
-    $fileName = Get-Item "$BasePath/$_"
+    $fileName = $_
+    "Reading localization file: $fileName" | Out-Host
     $fileContents = Get-Content $fileName -Raw # removes the BOM
     $fileContents += "`n" # add a trailing newline
+    "Reading localization file: $fileName" | Out-Host
     $fileContents | Out-File `
         -FilePath $fileName `
         -Encoding UTF8NoBOM `
